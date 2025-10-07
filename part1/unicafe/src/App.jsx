5022a61f0fb34fc99c6d1e81ccb187ae355af384
profsibz3090 <sibandadonty@gmail.com>
2025-10-07 12:51:57 +0200

    unicafe step 2 done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,15 +8,15 @@ const App = () => {
 
   const handleGoodStats = () => {
     setGood(good + 1);
-  }
+  };
 
   const handleNeutralStats = () => {
     setNeutral(neutral + 1);
-  }
+  };
 
   const handleBadStats = () => {
     setBad(bad + 1);
-  }
+  };
 
   return (
     <div>
@@ -30,6 +30,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {good + neutral + bad}</p>
+      <p>average {(good + neutral + bad) / 3}</p>
+      <p>positive {(good / (good + neutral + bad)) * 100}%</p>
     </div>
   );
 };
