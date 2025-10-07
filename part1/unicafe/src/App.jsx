7191604621a3669b944ddd2a953e9ff3085b9f34
profsibz3090 <sibandadonty@gmail.com>
2025-10-07 09:26:56 +0200

    modified unicafe step 1 code adding the logic

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,16 +1,37 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGoodStats = () => {
+    setGood(good + 1);
+  }
+
+  const handleNeutralStats = () => {
+    setNeutral(neutral + 1);
+  }
+
+  const handleBadStats = () => {
+    setBad(bad + 1);
+  }
 
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      <div>
+        <button onClick={handleGoodStats}>good</button>
+        <button onClick={handleNeutralStats}>neutral</button>
+        <button onClick={handleBadStats}>bad</button>
+      </div>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
