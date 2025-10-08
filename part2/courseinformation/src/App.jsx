2569c8137ba3102fc81be08def29ca85e0999a45
profsibz3090 <sibandadonty@gmail.com>
2025-10-08 08:35:21 +0200

    separate module step 10 done

--- part2/courseinformation/src/App.jsx
+++ part2/courseinformation/src/App.jsx
@@ -1,38 +1,4 @@
-const Header = (props) => <h1>{props.course}</h1>;
-
-const Content = (props) => {
-  const total = props.parts.reduce((a, b) => {
-    return a + b.exercises;
-  }, 0);
-
-  return (
-    <div>
-      {props.parts.map((part) => (
-        <Part key={part.id} part={part} />
-      ))}
-      <Total total={total} />
-    </div>
-  );
-};
-
-const Part = (props) => (
-  <p>
-    {props.part.name} {props.part.exercises}
-  </p>
-);
-
-const Total = (props) => (
-  <p style={{ fontWeight: "bold" }}>total of {props.total} exercises</p>
-);
-
-const Course = ({ course }) => {
-  return (
-    <>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-    </>
-  );
-};
+import Course from "./components/Course";
 
 const App = () => {
   const courses = [
