9c6a1fea9952f176b723b51d0a4cf300f6ef11df
profsibz3090 <sibandadonty@gmail.com>
2025-10-12 12:19:34 +0200

    phonebook backend step 8 done

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -3,7 +3,10 @@ var morgan = require('morgan')
 
 app = express()
 app.use(express.json())
-app.use(morgan('tiny'))
+morgan.token("data", (req, res) => {
+    return JSON.stringify(req.body);
+})
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :data"))
 const PORT = 3001;
 
 const persons = [
