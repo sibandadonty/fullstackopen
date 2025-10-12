095cc1255dbe31f1a6164ef7b2ceabc5ed0c81db
profsibz3090 <sibandadonty@gmail.com>
2025-10-12 08:48:12 +0200

    phonebook backend step 2 done

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -30,6 +30,14 @@ app.get("/api/persons", (request, response) => {
     response.json(persons);
 }) 
 
+app.get("/info", (request, response) => {
+    const res = `
+      <p>Phonebook has information for ${persons.length} people</p>
+      <p>${new Date()}</p>
+    `
+    response.send(res);
+})
+
 app.listen(PORT, () => {
     console.log(`server listening on port ${PORT}`);
 })
