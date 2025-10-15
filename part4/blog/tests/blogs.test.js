a37f14de7f63c5460fee2f19595a1faa60080557
profsibz3090 <sibandadonty@gmail.com>
2025-10-15 10:57:03 +0200

    Blog List Tests, step 2 done

--- part4/blog/tests/blogs.test.js
+++ part4/blog/tests/blogs.test.js
@@ -1,4 +1,4 @@
-const { after, test, beforeEach } = require("node:test");
+const { after, test, beforeEach, describe } = require("node:test");
 const assert = require("node:assert");
 const mongoose = require("mongoose");
 const request = require("supertest");
@@ -13,15 +13,24 @@ beforeEach(async () => {
     await Blog.insertMany(blogs);
 })
 
-test("GET /api/blogs", async () => {
+describe("Blog Tests", async () => {
+    
+
+    test("GET /api/blogs", async () => {
     api
     .get("/api/blogs")
     .expect(200)
     .expect("Content-Type", /application\/json/)
-
     const response = await api.get("/api/blogs");
+    
     assert.strictEqual(response.body.length, blogs.length);
     
+    })
+
+    test(" unique identifier property of the blog posts is named id", async () => {
+        const response = await api.get("/api/blogs");
+        assert(response.body[0].id !== undefined);
+    })
 })
 
 
