5c28750be20ea74bb47087f855706a63e80c6339
profsibz3090 <sibandadonty@gmail.com>
2025-10-21 08:03:35 +0200

    Blog List Frontend, step 7 done

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,27 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [viewDetails, setViewDetails] = useState(false);
+  const handleSetView = () => {
+    setViewDetails((prev) => !prev);
+  };
+  return (
+    <div>
+      <div className="back-border">
+        {blog.title} {blog.author}{" "}
+        <button onClick={handleSetView}>{viewDetails ? "hide" : "view"}</button>
+      </div>
+      {viewDetails && (
+        <div className="back-border">
+          <p>{blog.url}</p>
+          <p>
+            likes {blog.likes} <button>like</button>
+          </p>
+          <p>{blog.author}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
