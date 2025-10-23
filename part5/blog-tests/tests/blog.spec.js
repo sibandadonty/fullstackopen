5c6147b551fa5b57e002fa5005a8f0e143332884
profsibz3090 <sibandadonty@gmail.com>
2025-10-23 13:26:12 +0200

    Blog List End To End Testing, step 2 done

--- part5/blog-tests/tests/blog.spec.js
+++ part5/blog-tests/tests/blog.spec.js
@@ -8,4 +8,21 @@ describe('Blog app', () => {
   test('Login form is shown', async ({ page }) => {
     await expect(page.getByRole("button", {name: 'login'})).toBeVisible()
   })
+
+   describe('Login', () => {
+  
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.getByLabel("username").fill("root")
+      await page.getByLabel("password").fill("qwerty123")
+      await page.getByRole("button", {name: 'login'}).click()
+      await expect(page.getByText("admin is logged in")).toBeVisible()
+     })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await page.getByLabel("username").fill("root")
+      await page.getByLabel("password").fill("wrong")
+      await page.getByRole("button", {name: 'login'}).click()
+      await expect(page.getByText("admin is logged in")).not.toBeVisible()
+    })
+  })
 })
