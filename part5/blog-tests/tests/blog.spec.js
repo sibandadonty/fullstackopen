4bd05a011b6f610198ad9d04a10fc2ea9a4c562d
profsibz3090 <sibandadonty@gmail.com>
2025-10-23 11:53:52 +0200

    Blog List End To End Testing, step 1 done

---
+++ part5/blog-tests/tests/blog.spec.js
@@ -0,0 +1,11 @@
+const { test, expect, beforeEach, describe } = require('@playwright/test')
+
+describe('Blog app', () => {
+  beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:5173')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    await expect(page.getByRole("button", {name: 'login'})).toBeVisible()
+  })
+})
