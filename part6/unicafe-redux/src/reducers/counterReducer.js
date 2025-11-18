57b5b9b3f524f0b545dd9bd1eed94450acb2344f
profsibz3090 <sibandadonty@gmail.com>
2025-11-18 14:10:00 +0200

    Unicafe Revisited, step2 done

--- part6/unicafe-redux/src/reducers/counterReducer.js
+++ part6/unicafe-redux/src/reducers/counterReducer.js
@@ -10,7 +10,7 @@ const counterReducer = (state = initialState, action) => {
     case 'GOOD':
       return {...state, good: state.good + 1}
     case 'OK':
-      return {...state, ok: state.good + 1}
+      return {...state, ok: state.ok + 1}
     case 'BAD':
       return {...state, bad: state.bad + 1}
     case 'RESET':
